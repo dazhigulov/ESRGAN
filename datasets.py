d92644696d30a4b6f830d08d680f74fb4a5b696c
dazhigulov <noobfromkz@gmail.com>
2021-03-07 22:05:00 -0800

    training rdy

--- datasets.py
+++ datasets.py
@@ -2,16 +2,19 @@ import glob
 import random
 import os
 import numpy as np
-
+import cv2 #***Added***#
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
+from random import randrange
+import torchvision.transforms.functional as TF
+
 
 # Normalization parameters for pre-trained PyTorch models
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
-
+SAMPLING = [Image.BICUBIC, Image.BILINEAR, Image.NEAREST]
 
 def denormalize(tensors):
     """ Denormalizes image tensors using mean and std """
@@ -21,30 +24,44 @@ def denormalize(tensors):
 
 
 class ImageDataset(Dataset):
-    def __init__(self, root, hr_shape):
-        hr_height, hr_width = hr_shape
+    def __init__(self, root, hr_shape, rand_crop_w=512, rand_crop_h=512):
+        hr_height, hr_width = hr_shape #can ignore this, not used
         # Transforms for low resolution images and high resolution images
         self.lr_transform = transforms.Compose(
             [
-                transforms.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
                 transforms.ToTensor(),
-                transforms.Normalize(mean, std),
+                transforms.Resize((rand_crop_w // 2, rand_crop_h // 2), SAMPLING[randrange(0,len(SAMPLING))]),
+                transforms.Resize((rand_crop_w // 2, rand_crop_h // 2), SAMPLING[randrange(0,len(SAMPLING))]),
+                transforms.Resize((rand_crop_w // 2, rand_crop_h // 2), SAMPLING[randrange(0,len(SAMPLING))]),
+                #transforms.Normalize(mean, std),
             ]
         )
         self.hr_transform = transforms.Compose(
             [
-                transforms.Resize((hr_height, hr_height), Image.BICUBIC),
                 transforms.ToTensor(),
-                transforms.Normalize(mean, std),
+                #transforms.Resize((hr_height, hr_height), Image.BICUBIC),
+                #transforms.Normalize(mean, std),
             ]
         )
 
         self.files = sorted(glob.glob(root + "/*.*"))
 
     def __getitem__(self, index):
-        img = Image.open(self.files[index % len(self.files)])
-        img_lr = self.lr_transform(img)
-        img_hr = self.hr_transform(img)
+        #img = Image.open(self.files[index % len(self.files)])
+        img_orig = cv2.imread(self.files[index % len(self.files)],\
+              cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+        x = randrange(0,4320-512)
+        y = randrange(0,7680-512)
+        xx = x+512
+        yy = y+512
+        final_img = img_orig[x:xx,y:yy]
+        #print("ORIG SIZE X: "+str(len(img_orig[:][0])))
+        #print("ORIG SIZE Y: "+str(len(img_orig[0][:])))
+        #print(final_img.shape)
+        #print("x = "+str(x)+", y = "+str(y))
+        #print("\n\nORIG SIZE: "+str(img_orig.shape)+". POINT: "+str(x)+","+str(y)+". IMAGE SIZE: "+str(final_img.shape)+"\n\n")
+        img_lr = self.lr_transform(final_img)
+        img_hr = self.hr_transform(final_img)
 
         return {"lr": img_lr, "hr": img_hr}
 
