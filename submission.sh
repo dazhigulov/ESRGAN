afc71925c60bf051f7f8c9faa7edd2cccfae550b
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2021-04-21 11:43:40 -0700

    update submission.sh

--- submission.sh
+++ submission.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=0:3:0
+#SBATCH --time=72:0:0
 #SBATCH --account=def-mahsa77
 #SBATCH --job-name=4k8k
 #SBATCH --ntasks=1
@@ -7,14 +7,13 @@
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --output=out_file.out
+#SBATCH --output=%j-%x.out
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-type=fail         # send email if job fails
-#SBATCH --mail-user=mohamad.ol95@gmail.com
+#SBATCH --mail-user=weskerlyw@gmail.com
 
 
-DATA_DIR='/home/yuweiluo/projects/def-mahsa77/dataset/8K/'
 
 module load python/3.6
 
@@ -26,6 +25,6 @@ pip install -U pip
 pip install -U setuptools
 pip install --no-index opencv-python
 
-python esrgan.py --batch_size 8 --hr_height 64 --hr_width 64 --dataset_path $DATA_DIR
+python esrgan.py --batch_size 4 --hr_height 512 --hr_width 512 --channels 3 --dataset_path '/home/yuweiluo/projects/def-mahsa77/dataset/8K/'
 
 
