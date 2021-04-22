4accbeced647e57ce707ef0c03caa34dca6f48f8
Dias Azhigulov <37943216+dazhigulov@users.noreply.github.com>
2021-04-22 02:42:01 -0700

    Add files via upload
    Training the latest model

--- datasets.py
+++ datasets.py
@@ -33,11 +33,13 @@ class ImageDataset(Dataset):
             [
                 transforms.ToPILImage(),
                 transforms.RandomCrop(hr_shape),
+                transforms.ToTensor()
             ]
         )
         
         self.lr_transform = transforms.Compose(
             [   
+                transforms.ToPILImage(),
                 transforms.Resize((hr_height // 2, hr_width // 2), SAMPLING[randrange(0,len(SAMPLING))]),
                 transforms.Resize((hr_height // 4, hr_width // 4), SAMPLING[randrange(0,len(SAMPLING))]),
                 transforms.Resize((hr_height // 8, hr_width // 8), SAMPLING[randrange(0,len(SAMPLING))]),
@@ -47,6 +49,7 @@ class ImageDataset(Dataset):
         
         self.hr_transform = transforms.Compose(
             [
+                transforms.ToPILImage(),
                 transforms.ToTensor()
             ]
         )
@@ -61,16 +64,17 @@ class ImageDataset(Dataset):
         #Read EXR file with CV2
         img = cv2.imread(self.files[index],\
               cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
-        print("Original image shape: "+str(img.shape))
+        #print("Original image shape: "+str(img.shape))
 
         #Downsample
         img = self.crop_transform(transforms.ToTensor()(img))
-        print("Cropped image shape: "+str(img.size))
+        print("Cropped image shape: "+str(img.shape))
+        #print("Cropped image shape after permutation: "+str(img_lr.shape))
         img_lr = self.lr_transform(img)
-        img_lr = img_lr.permute(1,2,0)
+        #img_lr = img_lr.permute(1,2,0)
         print("LR image shape: "+str(img_lr.size()))
-        img_hr = self.hr_transform(img)
-        img_hr = img_hr.permute(1,2,0)
+        #img_hr = self.hr_transform(img)
+        img_hr = img
         print("HR image shape: "+str(img_hr.size()))
 
         return {"lr": img_lr, "hr": img_hr}
--- submission.sh
+++ submission.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#SBATCH --time=0:3:0
+#SBATCH --time=1-59:0
 #SBATCH --account=def-mahsa77
 #SBATCH --job-name=4k8k
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
-#SBATCH --output=out_file.out
+#SBATCH --mem=48G
+#SBATCH --output=%j-%x.out
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-type=fail         # send email if job fails
-#SBATCH --mail-user=noobfromkz@gmail.com
+#SBATCH --mail-user=weskerlyw@gmail.com
 
 
-DATA_DIR='/home/adias/projects/def-mahsa77/dataset/8K/'
 
 module load python/3.6
 
@@ -26,6 +25,4 @@ pip install -U pip
 pip install -U setuptools
 pip install --no-index opencv-python
 
-python esrgan.py --batch_size 1 --hr_height 64 --hr_width 64 --dataset_path $DATA_DIR
-
-
+python esrgan.py --batch_size 4 --hr_height 512 --hr_width 512 --channels 3 --dataset_path '/home/molyaiy/projects/def-mahsa77/dataset/8K/'
